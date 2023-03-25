412d9707675817259a38f2819446ad2abf82269c
xbwtyz <115103296+xbwtyz@users.noreply.github.com>
2023-03-24 22:15:08 -0700

    Update tile_anal.py
    Script now splices sprites as PNGs in the folder where the source sheet is located

--- tile_anal.py
+++ tile_anal.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import os
 import sys
 
 def find_grid_color(image):
@@ -49,6 +50,31 @@ def analyze_tileset(image_path):
         'tiles_x': tiles_x,
         'tiles_y': tiles_y,
     }
+def save_individual_tiles(image_path, result):
+    try:
+        image = Image.open(image_path)
+    except IOError:
+        print(f"Error opening the image file: {image_path}")
+        sys.exit(1)
+
+    output_dir = os.path.dirname(image_path)
+    base_name = os.path.splitext(os.path.basename(image_path))[0]
+    tile_width = result['tile_width']
+    tile_height = result['tile_height']
+    spacing_x = result['spacing_x']
+    spacing_y = result['spacing_y']
+    tiles_x = result['tiles_x']
+    tiles_y = result['tiles_y']
+
+    for y in range(tiles_y):
+        for x in range(tiles_x):
+            tile = image.crop((
+                x * (tile_width + spacing_x),
+                y * (tile_height + spacing_y),
+                x * (tile_width + spacing_x) + tile_width,
+                y * (tile_height + spacing_y) + tile_height,
+            ))
+            tile.save(os.path.join(output_dir, f"{base_name}_tile_{y}_{x}.png"), "PNG")
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -64,3 +90,7 @@ if __name__ == '__main__':
     print(f"Spacing in Y: {result['spacing_y']}")
     print(f"Number of tiles in X: {result['tiles_x']}")
     print(f"Number of tiles in Y: {result['tiles_y']}")
+
+    save_individual_tiles(image_path, result)
+    print("Tiles saved successfully.")
+    
